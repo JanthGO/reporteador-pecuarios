a1e12fda4cad4c78c1fd4a9b04055e90291dd7ba
Antonio <anthoniogaort@gmail.com>
2026-09-24 13:00:44 -0600

    Rutas - Configuración Guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,22 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LayoutComponent } from './layout/layout.component';
 import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
+  {
+    path: '',
+    component: LayoutComponent,
+    canActivate: [authGuard],
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./pages/dashboard/dashboard.component').then((m) => m.DashboardComponent),
+      },
+    ],
+  },
   { path: '**', redirectTo: 'dashboard', pathMatch: 'full' },
 ];
